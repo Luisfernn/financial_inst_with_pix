b3ef81a31d1b0332eb66a59f9cf2b3ae4c72bac1
Luis Fernando <luisfernandowubi@gmail.com>
2026-04-12 11:22:01 -0300

    feat(etl): cria o script de extração dos dados

---
+++ src/extract_data.py
@@ -0,0 +1,42 @@
+import requests
+import json
+
+from pathlib import Path
+
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+url = "https://brasilapi.com.br/api/pix/v1/participants"
+
+def extract_pix_data(url: str): --> list:
+    """
+    Extrai os dados de PIX a partir da API e salva em um arquivo JSON.
+    """
+
+
+    response = requests.get(url)
+
+    data = response.json()
+
+    if response.status_code != 200:
+        logging.error(f"Falha na requisição para {url}. Status code: {response.status_code}")
+        return []
+
+    if not data:
+        logging.warning(f"Nenhum dado encontrado em {url}.")
+        return []    
+
+
+    output_path = 'data' / 'pix_data.json' 
+    outputh_dir = Path(output_path).parent
+    outputh_dir.mkdir(parents=True, exist_ok=True)
+
+    with open (output_path, 'w') as f:
+        json.dump(data, f, indent=4)
+
+    logging.info(f"Dados extraídos e salvos em {output_path}")    
+
+    return data
+
+
+extract_pix_data(url)
