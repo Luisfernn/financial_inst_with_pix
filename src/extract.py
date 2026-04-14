d6b2c76dbec7b1fbaa84fd894d7ebabb716018fc
Luis Fernando <luisfernandowubi@gmail.com>
2026-04-14 07:32:19 -0300

    fix(etl): corrige path do arquivo de output e a ordem de verificação do status code
    Corrigi a ordem do if, colocando a verificação do status code antes da leitura do arquivo para impedir que o código quebrasse.

--- src/extract.py
+++ src/extract.py
@@ -16,20 +16,20 @@ def extract_pix_data(url: str) -> list:
 
     response = requests.get(url)
 
-    data = response.json()
-
     if response.status_code != 200:
         logging.error(f"Falha na requisição para {url}. Status code: {response.status_code}")
         return []
+
+    data = response.json()
 
     if not data:
         logging.warning(f"Nenhum dado encontrado em {url}.")
         return []    
 
 
-    output_path = 'data' / 'pix_data.json' 
-    outputh_dir = Path(output_path).parent
-    outputh_dir.mkdir(parents=True, exist_ok=True)
+    output_path = Path('data') / 'pix_data.json' 
+    output_dir = Path(output_path).parent
+    output_dir.mkdir(parents=True, exist_ok=True)
 
     with open (output_path, 'w') as f:
         json.dump(data, f, indent=4)
