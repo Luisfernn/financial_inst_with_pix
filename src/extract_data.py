8c3e5c1b6674f707c6b45c14ca2dd06a0e7f8206
Luis Fernando <luisfernandowubi@gmail.com>
2026-04-16 11:18:21 -0300

    feat(etl): adiciona função para extrair dados de outra fonte e realizar data enrichment na pipeline
    Essa nova base de dados da API Olinda do Banco Central faz um merge na base original, categorizando automaticamente as instituições financeiras por segmento e natureza jurídica.

--- src/extract_data.py
+++ src/extract_data.py
@@ -7,6 +7,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 url_apibrasil = "https://brasilapi.com.br/api/pix/v1/participants"
+url_bcb = "https://olinda.bcb.gov.br/olinda/servico/BcBase/versao/v2/odata/EntidadesSupervisionadas(dataBase=@dataBase)?@dataBase='04-16-2026'&$top=10000&$format=json&$select=nomeDoPais,nomeEntidadeInteresse,codigoCNPJ8,codigoTipoEntidadeSupervisionada,descricaoTipoEntidadeSupervisionada,nomeFantasia,indicadorEsferaPublica"
 
 def extract_pix_data(url: str) -> list:
     """
@@ -39,6 +40,39 @@ def extract_pix_data(url: str) -> list:
     except requests.exceptions.RequestException as e:
         logging.error(f"Erro ao extrair dados da API: {e}")
         return []
+    
+
+def extract_bcb_reference(url: str) -> list:
+    """
+    Extrai os dados de referência das instituições supervisionadas pelo Banco Central e salva em um arquivo JSON.
+    """
+
+    try:
+        logging.info(f"Extraindo dados da API do Banco Central: {url_bcb}...")
+
+        response = requests.get(url_bcb, timeout=15)
+        response.raise_for_status()
+
+        data = response.json().get('value', [])
+
+        if not data:
+            logging.warning("Nenhum dado encontrado na resposta da API.")
+            return []
+        
+        output_path = Path('data') / 'bcb_reference.json'
+        output_dir = Path(output_path).parent
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+        with open (output_path, 'w', encoding='utf-8') as f:
+            json.dump(data, f, indent=4, ensure_ascii=False)
+
+        logging.info(f"Dados extraídos e salvos em {output_path}")
+        return data
+    
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Erro ao extrair dados da API: {e}")
+        return []
 
 
 extract_pix_data(url_apibrasil)
+extract_bcb_reference(url_bcb)
