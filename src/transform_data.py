d2e71dd52ea8f9860ff7aacb61a4816a9272c813
Luis Fernando <luisfernandowubi@gmail.com>
2026-04-18 06:50:11 -0300

    feat(etl): cria função para extrair dados da segunda fonte e refatora a primeira função
    - Corrige variáis globais.
    
    - Remove variáveis locais.

--- src/transform_data.py
+++ src/transform_data.py
@@ -5,27 +5,46 @@ import json
 import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-path_name = Path(__file__).parent.parent / 'data' / 'pix_data.json'
+path_name_pix = Path(__file__).parent.parent / 'data' / 'pix_data.json'
+path_name_bcb = Path(__file__).parent.parent / 'data' / 'bcb_reference.json'
 
 
-def create_dataframe(path_name: str) -> pd.DataFrame:
+def create_pix_dataframe(path: str) -> pd.DataFrame:
     """
     Cria um DataFrame a partir do arquivo JSON contendo os dados de PIX.
     """
 
-    logging.info(f"Criando DataFrame a partir do arquivo JSON...")
-    path = path_name
+    logging.info(f"Criando DataFrame do PIX a partir do arquivo JSON...")
 
-
-    if not path_name.exists():
-        raise FileNotFoundError(f"O arquivo {path_name} não foi encontrado.")
+    if not path_name_pix.exists():
+        raise FileNotFoundError(f"O arquivo {path} não foi encontrado.")
     
-    with open(path) as f:
+    with open(path, 'r', encoding='utf-8') as f:
         data = json.load(f)
 
-    df = pd.json_normalize(data)
+    df_pix = pd.json_normalize(data)
 
-    logging.info(f"DataFrame criado com sucesso. Número de linhas: {len(df)}, Número de colunas: {len(df.columns)}")  
+    logging.info(f"DataFrame do PIX criado com sucesso. Número de linhas: {len(df_pix)}, Número de colunas: {len(df_pix.columns)}")  
 
-    return df
+    return df_pix
+
+
+def create_bcb_dataframe(path: str) -> pd.DataFrame:
+    """
+    Cria um DataFrame a partir do arquivo JSON contendo os dados de referência das instituições supervisionadas pelo Banco Central.
+    """
+
+    logging.info(f"Criando DataFrame referência do Banco Central a partir do arquivo JSON...")
+
+    if not path_name_bcb.exists():
+        raise FileNotFoundError(f"O arquivo {path} não foi encontrado.")
+    
+    with open(path, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    df_ref_bcb = pd.json_normalize(data)
+
+    logging.info(f"DataFrame do Banco Central criado com sucesso. Número de linhas: {len(df_ref_bcb)}, Número de colunas: {len(df_ref_bcb.columns)}")    
+
+    return df_ref_bcb
     
